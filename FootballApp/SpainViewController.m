076d0a61bccecc1ccce2099dc60e1e1824c6cce9
FredrikGhofran <fredrik_ghofran@hotmail.com>
2014-06-16 16:36:19 +0200

    gets data from database

--- FootballApp/SpainViewController.m
+++ FootballApp/SpainViewController.m
@@ -10,9 +10,9 @@
 #import "MyCollectionViewCell.h"
 
 @interface SpainViewController ()
-@property(nonatomic)NSArray *titles;
-@property(nonatomic)NSArray *videos;
-@property(nonatomic)NSArray *descriptions;
+@property(nonatomic)NSMutableArray *titles;
+@property(nonatomic)NSMutableArray *videos;
+@property(nonatomic)NSMutableArray *descriptions;
 @property (weak, nonatomic) IBOutlet UICollectionView *collectionView;
 
 @end
@@ -30,9 +30,53 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    self.titles = @[@"Uruguay - Costa Rica",@"Chile - Australien",@"Spanien - Holland",@"Mexico - Kamerun",@"Brasilien - Kroatien"];
-    self.videos =@[@"l5VPRJ27Z74",@"Gv6sEfNBnSk",@"RKpFqOTIjW8",@"G2tKqDp8RSo",@"3O9NPBYxd-Q"];
-    self.descriptions = @[@"Costa Rica vinner och vänder ett 1-0 underläge till 1-3. Stor skräll från Costa Rica",@"Chile vinner säkert över Australien med 3-1",@"Holland vinner förvånansvärt stort över de regerande världsmästarna Spanien med 5-1, och kunde även gjort ett par till om de hade varit vassare i sina lägen.",@"Mexico vinner matchen enkelt med 1-0 även fast de får två mål bortdömda.",@"Brasilien vinner matchen med 3-1 efter två tveksamma beslut av dommaren. Kroatioen fick en tveksam straff dömd mot sig och ett bortdömt mål."];
+    self.titles = [[NSMutableArray alloc]init];
+    self.videos = [[NSMutableArray alloc]init];
+    self.descriptions = [[NSMutableArray alloc]init];
+    NSString *urlString = @"http://fredrikghofran.com/football/getAllVideos.php";
+    
+    
+    NSURL *URL = [NSURL URLWithString:urlString];
+    
+    NSURLRequest *request = [NSURLRequest requestWithURL:URL];
+    
+    NSURLSession *session = [NSURLSession sharedSession];
+    
+    
+    NSURLSessionDataTask *task = [session dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+        NSError *parseError;
+        NSArray *jsonArray = [NSJSONSerialization JSONObjectWithData:data options:kNilOptions error:&parseError];
+        for(int i = 0;i<jsonArray.count;i++)
+        {
+        
+            NSDictionary *dic = jsonArray[i];
+            NSLog(@"1");
+            NSLog(@"DIC = %@",dic);
+            [self.videos addObject:dic[@"videoText"]];
+            NSLog(@"2");
+            [self.titles addObject:dic[@"title"]];
+            NSLog(@"3");
+            [self.descriptions addObject:dic[@"description"]];
+            NSLog(@"4");
+            NSLog(@"DESCTIPTION = %@",self.descriptions[i]);
+            NSLog(@"5");
+            dispatch_async(dispatch_get_main_queue(),^{
+                [self.collectionView reloadData];
+                NSLog(@"6");
+            });
+        
+        }
+  
+    }];
+
+    
+    [task resume];
+//    self.titles = @[@"Uruguay - Costa Rica",@"Chile - Australien",@"Spanien - Holland",@"Mexico - Kamerun",@"Brasilien - Kroatien"];
+//    self.videos =@[@"qNoPy7CxvZc",@"Gv6sEfNBnSk",@"yXTFG97E4yY",@"G2tKqDp8RSo",@"dA2CuJUq8-Q"];
+//    self.descriptions = @[@"Costa Rica vinner och vänder ett 1-0 underläge till 1-3. Stor skräll från Costa Rica",@"Chile vinner säkert över Australien med 3-1",@"Holland vinner förvånansvärt stort över de regerande världsmästarna Spanien med 5-1, och kunde även gjort ett par till om de hade varit vassare i sina lägen.",@"Mexico vinner matchen enkelt med 1-0 även fast de får två mål bortdömda.",@"Brasilien vinner matchen med 3-1 efter två tveksamma beslut av dommaren. Kroatioen fick en tveksam straff dömd mot sig och ett bortdömt mål."];
+    
+    
+    
 }
 
 - (void)didReceiveMemoryWarning
@@ -47,8 +91,12 @@
 }
 -(NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section
 {
+    if (self.videos) {
+         return self.videos.count;
+    }else{
+        return 0;
+    }
     
-    return self.videos.count;
 }
 -(UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath
 {
@@ -59,16 +107,20 @@
     
 
     
+        cell.titleLabel.text =self.titles[indexPath.row];
 
+        
+
+        NSString *html = [NSString stringWithFormat:@"<object><param name=\"movie\" value=\"http://www.youtube.com/v/%@\"></param><embed src=\"http://www.youtube.com/v/%@\" type=\"application/x-shockwave-flash\"></embed></object>",self.videos[indexPath.row], self.videos[indexPath.row]];
+        
+        
+        [cell.webView loadHTMLString:html baseURL:nil];
     
-    cell.titleLabel.text =self.titles[indexPath.row];
     
-    NSString *html = [NSString stringWithFormat:@"<object><param name=\"movie\" value=\"http://www.youtube.com/v/%@\"></param><embed src=\"http://www.youtube.com/v/%@\" type=\"application/x-shockwave-flash\"></embed></object>",self.videos[indexPath.row], self.videos[indexPath.row]];
+        cell.description.text = self.descriptions[indexPath.row];
+        cell.description.editable = NO;
     
-    
-    [cell.webView loadHTMLString:html baseURL:nil];
-    cell.description.text = self.descriptions[indexPath.row];
-    cell.description.editable = NO;
+  
     //[cell.description setUserInteractionEnabled:NO];
     return cell;
 }
